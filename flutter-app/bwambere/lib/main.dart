032872ea8656bd3b516350e8b4261d7e509ca9e3
Mr-Ndi <onlythenotes@gmail.com>
2025-03-30 14:31:51 +0200

    Hot Reloading Stateless Widgets! - Flutter Friday 7

--- flutter-app/bwambere/lib/main.dart
+++ flutter-app/bwambere/lib/main.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Center(
-            child: Text("Sample title"),
-          ),
-          backgroundColor:Colors.deepPurple[400],
+          title: const Center(child: Text("Sample title")),
+          backgroundColor: Colors.deepPurple[100],
         ),
         body: Center(
           child: const Image(image: AssetImage('images/image.jpeg')),
         ),
-        backgroundColor: Colors.deepPurple[100]
+        backgroundColor: Colors.deepPurple[500],
       ),
-    ),
-  );
+    );
+  }
 }
