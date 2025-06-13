c0cbdd63e000c29a6793a1b7fb1d24641f0adace
Mr-Ndi <onlythenotes@gmail.com>
2025-06-13 16:00:14 +0200

    Inhertance

--- Dart_tp_Futter/second_day.dart
+++ Dart_tp_Futter/second_day.dart
@@ -6,8 +6,17 @@ class Animal {
     print('$name makes sounds');
   }
 }
+class Dog extends Animal{
+  Dog(String name): super(name);
+
+  void speak() {
+    print('$name make sounds');
+  }
+}
 void main()
 {
   var sample = Animal('cows');
+  var sample2 = Dog('kamazage');
   sample.speak();
+  sample2.speak();
 }
