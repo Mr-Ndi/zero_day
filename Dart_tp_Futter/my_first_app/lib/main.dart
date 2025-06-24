e17e1073c8ee53776d4a041034f840df85be4da5
Mr-Ndi <onlythenotes@gmail.com>
2025-06-24 12:43:47 +0200

    Day7 | to do shee

--- Dart_tp_Futter/my_first_app/lib/main.dart
+++ Dart_tp_Futter/my_first_app/lib/main.dart
@@ -4,18 +4,52 @@ void main() {
   runApp(MaterialApp(home: MyHomePage()));
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _controller = TextEditingController();
+  final List<String> _plans = [];
+
+  void _addPlan() {
+    final text = _controller.text;
+    if (text.isNotEmpty) {
+      setState(() {
+        _plans.add(text);
+        _controller.clear(); // clear input after adding
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Plan Pulse')), // Top navigation bar
+      appBar: AppBar(title: Text('Plan Pulse')),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: TextField(
-          decoration: InputDecoration(
-            labelText: 'Enter your plan',
-            border: OutlineInputBorder(),
-          ),
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                labelText: 'Enter your plan',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: _addPlan, child: Text('Add Plan')),
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _plans.length,
+                itemBuilder: (context, index) {
+                  return ListTile(title: Text(_plans[index]));
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
