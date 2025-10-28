9bfc27bdbef2f440539dbca3f2b1f7c6b5540e16
Mr-Ndi <onlythenotes@gmail.com>
2025-10-28 10:23:24 +0200

    Pydantic classes in controllers

--- Fasting/controllers/book_controller.py
+++ Fasting/controllers/book_controller.py
@@ -1,5 +1,6 @@
 from fastapi import HTTPException
 from models.book_model import Book
+from middleware.schemas import BookCreate, BookUpdate
 from service.book_service import insert_book, get_book_by_id, get_all_books, delete_book, update_book
 
 def get_book(book_id: int):
@@ -14,7 +15,7 @@ def get_books():
         raise HTTPException(status_code=404, detail="Books not found")
     return books
 
-def create_book(book: Book):
+def create_book(book: BookCreate):
     new_book = insert_book(book=book)
     if not new_book:
         raise HTTPException(status_code=404, detail="Book not found")
@@ -24,7 +25,7 @@ def remove_book(book_id: int):
     result = delete_book(book_id=book_id)
     return result
 
-def modify_book(book_id: int, updated_book: Book):
+def modify_book(book_id: int, updated_book: BookUpdate):
     if not book_id:
         raise HTTPException(status_code=404, detail="Book not found")
     book = get_book_by_id(book_id=book_id)
