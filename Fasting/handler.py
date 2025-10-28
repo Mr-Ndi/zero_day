52c976be8b5482ecd0d32e89edd7813f1a960df5
Mr-Ndi <onlythenotes@gmail.com>
2025-10-28 20:50:34 +0200

    Fix: Async database handler

--- Fasting/handler.py
+++ Fasting/handler.py
@@ -1,10 +1,10 @@
 from sqlmodel import SQLModel, create_engine
-from models.book_model import Book
+from sqlmodel import create_async_engine
 
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
-engine = create_engine(sqlite_url, echo=True)
+engine = create_async_engine(sqlite_url, echo=True)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
